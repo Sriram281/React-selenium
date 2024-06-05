9eb0f853f50c9a10fdb5e7a4431536611cd8344a
Sriram281 <sriramc2810@gmail.com>
2024-06-05 11:10:33 +0530

    Your commit message

---
+++ src/Login_check.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login_check = () => {
+  const nav = useNavigate();
+  const storage = window.localStorage.getItem("studentID");
+  useEffect(() => {
+    if (storage) {
+    } else {
+      nav("/");
+    }
+  }, []);
+  return <></>;
+};
+
+export default Login_check;
