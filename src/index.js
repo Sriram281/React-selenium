41a95f033223c8d31d9d0cd1769bf327ead84705
Sriram281 <sriramc2810@gmail.com>
2024-06-06 20:02:12 +0530

    Update index.js hide login_check

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./Footer";
-import Login_check from "./Login_check";
+// import Login_check from "./Login_check";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Login_check />
+      // <Login_check />
       <App />
 
       <Footer />
