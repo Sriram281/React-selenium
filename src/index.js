6c97b067ec95d7f77b9a10ce5faff44014b2dc14
Sriram281 <sriramc2810@gmail.com>
2024-06-06 20:04:40 +0530

    Update index.js footer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 // import Login_check from "./Login_check";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,7 +14,7 @@ root.render(
       // <Login_check />
       <App />
 
-      <Footer />
+      // <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
